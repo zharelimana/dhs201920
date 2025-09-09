0b6489e2a1894ae9be6af71b28e273e30428a698
zharelimana <zacharieharelimana@gmail.com>
2025-09-09 18:33:32 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st 
 import pandas as pd
 # Welcome message
-st.title("Welcome to the Data Analysis dashboard portal")
+st.title("Welcome to the DHS Data Analysis dashboard portal")
